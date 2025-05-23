a97a3c2d3d6c8644648da30f0aeac48a077da08b
eduerdoy <eduardo.correap17@gmail.com>
2025-05-23 17:26:44 -0300

    +Alterações da planilha

--- server-tedplan/app/Controllers/Http/GestaoIndicadoresController.js
+++ server-tedplan/app/Controllers/Http/GestaoIndicadoresController.js
@@ -274,14 +274,8 @@ class GestaoIndicadoresController {
     return resGa;
   }
 
-
-  // Ver requisições com "console.log(request.all()) 
-  // return" 
-
   async addConselhoMunicipal({ request, response }) {
     try {
-      console.log(request.all())
-      return
       const { titulo, ano, id_municipio, operacao } = request.all();
 
       if (request.file("arquivo")) {
