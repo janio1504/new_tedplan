73cc2059bb66cea1b8bb237d8b239c94a4005749
janio Cesar <janiocesar.dev@gmail.com>
2025-12-29 18:14:33 -0300

    atualizacao-front-dashboard

--- server-tedplan/app/Models/Eixo.js
+++ server-tedplan/app/Models/Eixo.js
@@ -4,9 +4,21 @@
 const Model = use('Model')
 
 class Eixo extends Model {
+  static get connection() {
+    return 'tedplan_db';
+  }
 
-    static get connection() { return 'tedplan_db' }
+  static get table() {
+    return 'tedplan.eixos';
+  }
 
+  static get primaryKey() {
+    return 'id_eixo';
+  }
+
+  static get dates() {
+    return super.dates.concat(['created_at', 'updated_at']);
+  }
 }
 
 module.exports = Eixo
